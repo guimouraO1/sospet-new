4b2ab9a5dc32634408979a2ea194856765d88cae
guimouraO1 <guimoura.oliveira@gmail.com>
2024-01-10 16:44:51 -0300

    styling

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css',
+  styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent implements OnInit {
   constructor(
